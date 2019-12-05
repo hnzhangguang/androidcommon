9bdd50b5e91528f6f7b2aa792215742d2eff1ff0
zhangguang <zhangguang@yonyou.com>
2019-12-05 16:14:38 +0800

    总结Android总入口

--- app/src/main/java/com/yy/app/MainActivity.java
+++ app/src/main/java/com/yy/app/MainActivity.java
@@ -2,11 +2,9 @@ package com.yy.app;
 
 import android.content.ComponentName;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.yy.app.base.BaseActivity;
 import com.yy.app.broadcastreceiver.ReceiverActivity;
