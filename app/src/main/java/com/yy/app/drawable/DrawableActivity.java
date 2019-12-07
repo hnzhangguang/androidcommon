ebe0cc730a3e8ae9a0494229bfd091a75bae3aed
zhangguang <zhangguang@yonyou.com>
2019-12-07 14:01:48 +0800

    总结Android总入口

--- app/src/main/java/com/yy/app/drawable/DrawableActivity.java
+++ app/src/main/java/com/yy/app/drawable/DrawableActivity.java
@@ -2,18 +2,19 @@ package com.yy.app.drawable;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.app.logger.LogUtils;
+import com.app.logger.PrettyFormatStrategy;
 import com.cunoraz.gifview.library.GifView;
 import com.squareup.picasso.Picasso;
 import com.yy.app.R;
 import com.yy.app.base.BaseActivity;
 import com.yy.app.base.Logger;
+
+import org.litepal.util.LogUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,7 +35,11 @@ public class DrawableActivity extends BaseActivity {
     GifView gifView;
     Button btn_picasso;
     ImageView iv_picasso;
-    private static final String photo_url = "http://photocdn.sohu.com/20110927/Img320705637.jpg";
+
+    Button btn_glide;
+    ImageView iv_glide;
+    private static final String photo_url = "http://photocdn.sohu" +
+            ".com/20110927/Img320705637.jpg";
 
     @Override
     public void initContentViewXml() {
@@ -45,9 +50,15 @@ public class DrawableActivity extends BaseActivity {
     public void initView() {
         super.initView();
         imageView3 = findViewById(R.id.imageView3);
+
         iv_photo = findViewById(R.id.iv_photo);
+
         btn_picasso = findViewById(R.id.btn_picasso);
         iv_picasso = findViewById(R.id.iv_picasso);
+
+        iv_glide = findViewById(R.id.iv_glide);
+        btn_glide = findViewById(R.id.btn_glide);
+
         gifView = findViewById(R.id.gifView);
 
 
@@ -56,7 +67,7 @@ public class DrawableActivity extends BaseActivity {
             // 获取图片流
             InputStream is = getAssets().open("photoview.jpg");
             Bitmap bm = BitmapFactory.decodeStream(is);
-//            imageView3.setImageBitmap(bm);
+            //            imageView3.setImageBitmap(bm);
             iv_photo.setImageBitmap(bm);
         } catch (IOException e) {
             Logger.e(e);
@@ -78,15 +89,79 @@ public class DrawableActivity extends BaseActivity {
     public void initListener() {
         super.initListener();
 
+        btn_glide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 常规基本用法
+                //                Glide.with(DrawableActivity.this).load(photo_url)
+                //                .into(iv_glide);
+
+                // 加载文件
+                InputStream is = null;
+                try {
+                    is = getAssets().open("photoview.jpg");
+                    Bitmap bm = BitmapFactory.decodeStream(is);
+                    // 加载本地图片
+                    //                    File file = new File(is);
+                    //                    Glide.with(DrawableActivity.this).load(file)
+                    //                    .into
+                    //                    (imageView);
+
+                    // 加载二进制流
+                    byte[] image = null;
+                    //                    Glide.with(DrawableActivity.this).load(image)
+                    //                    .into
+                    //                    (iv_glide);
+
+                    //// 加载Uri对象
+                    //Uri imageUri = getImageUri();
+                    //Glide.with(this).load(imageUri).into(imageView);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //// 加载应用资源
+                int resource = R.drawable.add;
+                //                Glide.with(DrawableActivity.this).load(resource).into
+                //                (iv_glide);
+
+
+                //加载带有占位图
+                //                Glide.with(DrawableActivity.this)
+                //                        .load(photo_url)
+                //                        .placeholder(R.drawable.add)
+                //                        .into(iv_glide);
+
+
+                // 加载失败,放占位符
+                //                Glide.with(DrawableActivity.this).load(photo_url)
+                //                        .placeholder(R.drawable.add)
+                //                        .error(R.drawable.icon_toolbar)
+                //                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                //                        //关闭Glide的硬盘缓存机制
+                //                        .into(iv_glide);
+
+                //                LogUtils.e("aaaa", "fkd");
+
+                LogUtils.w("ddd");
+                PrettyFormatStrategy.methodCount = 3;
+                LogUtils.e("jfkljffj");
+
+            }
+        });
+
         // picasso 基本用法
         btn_picasso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // 基本用法
-//                Picasso.with(DrawableActivity.this)
-//                        .load(photo_url)
-//                        .into(iv_picasso);
+                //                Picasso.with(DrawableActivity.this)
+                //                        .load(photo_url)
+                //                        .into(iv_picasso);
 
                 // 固定大小
                 Picasso.with(DrawableActivity.this)
