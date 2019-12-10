d4918151cfd29583ec22a6f1366051f74bc41770
zhangguang <zhangguang@yonyou.com>
2019-12-10 20:13:53 +0800

    总结Android总入口

---
+++ app/src/main/java/function/geolocation/GeoLocationActivity.java
@@ -0,0 +1,44 @@
+package function.geolocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yy.app.R;
+import com.yy.app.base.BaseActivity;
+
+public class GeoLocationActivity extends BaseActivity {
+
+
+    @Override
+    public void initContentViewXml() {
+        setContentView(R.layout.activity_geo_location);
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+    }
+
+    @Override
+    public void initListener() {
+        super.initListener();
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
+
