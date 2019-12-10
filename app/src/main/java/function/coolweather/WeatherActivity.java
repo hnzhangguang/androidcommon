6fcd2a0f55ca804c8dbefcdf91961d44e7722be7
zhangguang <zhangguang@yonyou.com>
2019-12-10 19:07:01 +0800

    总结Android总入口

--- app/src/main/java/function/coolweather/WeatherActivity.java
+++ app/src/main/java/function/coolweather/WeatherActivity.java
@@ -237,6 +237,7 @@ public class WeatherActivity extends AppCompatActivity {
         weatherLayout.setVisibility(View.VISIBLE);
         Intent intent = new Intent(this, AutoUpdateService.class);
         startService(intent);
+        //
     }
 
 }
