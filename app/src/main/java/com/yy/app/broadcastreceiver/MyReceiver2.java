796e9651e43becfee52c538988c96fdd5e166fd5
zhangguang <zhangguang@yonyou.com>
2019-12-11 19:57:03 +0800

    DiskLruCache 的使用

--- app/src/main/java/com/yy/app/broadcastreceiver/MyReceiver2.java
+++ app/src/main/java/com/yy/app/broadcastreceiver/MyReceiver2.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.yy.app.mylibrary.Logger;
+import com.app.logger.LogUtil;
 
 
 public class MyReceiver2 extends BroadcastReceiver {
@@ -12,7 +12,7 @@ public class MyReceiver2 extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Logger.e("MyReceiver2 - onReceive");
+        LogUtil.e("MyReceiver2 - onReceive");
 
         abortBroadcast();  // 有序广播的时候, 优先级高的可以废弃掉优先级底的广播
     }
