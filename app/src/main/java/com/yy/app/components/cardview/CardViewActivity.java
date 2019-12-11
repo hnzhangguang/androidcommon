3e1b8a9a2ffe2294697dcee7b74ea7478657fd65
zhangguang <zhangguang@yonyou.com>
2019-12-11 09:32:02 +0800

    百度地图demo上传(一般so文件只用armeabi-v7a即可)

--- app/src/main/java/com/yy/app/components/cardview/CardViewActivity.java
+++ app/src/main/java/com/yy/app/components/cardview/CardViewActivity.java
@@ -8,6 +8,7 @@ import com.yy.app.R;
 
 /**
  * 1, Cardview继承自FrameLayout，所以子控件布局规则和FrameLayout的一样
+ * 2, card_view:cardCornerRadius="4dp"
  */
 public class CardViewActivity extends AppCompatActivity {
 
