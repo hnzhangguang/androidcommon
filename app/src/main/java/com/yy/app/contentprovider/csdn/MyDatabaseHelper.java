79b3d4ec5243679e4225a4b7e64883f6986bbb42
zhangguang <zhangguang@yonyou.com>
2019-12-27 21:00:42 +0800

    内容提供者 自定义

---
+++ app/src/main/java/com/yy/app/contentprovider/csdn/MyDatabaseHelper.java
@@ -0,0 +1,26 @@
+package com.yy.app.contentprovider.csdn;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+
+    public MyDatabaseHelper(@Nullable Context context, @Nullable String name,
+                            @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
