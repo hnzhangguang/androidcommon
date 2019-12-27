b33b65cddb8fbba9e51ea9a30f8eceb525848ac9
zhangguang <zhangguang@yonyou.com>
2019-12-27 14:28:26 +0800

    constraintLayout 的学习

---
+++ app/src/main/java/com/yy/app/components/ConstraintActivity.java
@@ -0,0 +1,32 @@
+package com.yy.app.components;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yy.app.R;
+
+
+/**
+ * 主要学习约束布局的使用:
+ * app:layout_constraintLeft_toLeftOf="parent"
+ * <p>
+ * 当我们希望控件A与控件B左侧对齐时，就可以使用该属性。
+ * app:layout_constraintLeft_toLeftOf="@id/viewB"
+ * 与之类似的还有几个属性：
+ * <p>
+ * layout_constraintRight_toLeftOf
+ * layout_constraintRight_toRightOf
+ * layout_constraintTop_toTopOf
+ * layout_constraintTop_toBottomOf
+ * layout_constraintBottom_toTopOf
+ * layout_constraintBottom_toBottomOf
+ * layout_constraintBaseline_toBaselineOf
+ */
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
